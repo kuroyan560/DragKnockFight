a24b08c94e6cff81f9177a66543234d64f84d23a
LC1A_07_オオイシカズノリ <k020g1151@g.neec.ac.jp>
2022-03-23 14:37:11 +0900

    空行

--- src/IntoTheAbyss/Player.h
+++ src/IntoTheAbyss/Player.h
@@ -62,7 +62,7 @@ public:
 	const int FIRST_SHOT_RECOIL_PARTICLE_TIMER = 10.0f;
 
 	//プレイヤーの向き
-	enum DRAW_DIR{ LEFT, RIGHT, DEFAULT = RIGHT, DIR_NUM }playerDir = DEFAULT;
+	enum DRAW_DIR { LEFT, RIGHT, DEFAULT = RIGHT, DIR_NUM }playerDir = DEFAULT;
 	//アニメーション統括
 	PlayerAnimation anim;
 
@@ -123,16 +123,16 @@ public:
 	~Player();
 
 	// 初期化処理
-	void Init(const Vec2<float>& INIT_POS);
+	void Init(const Vec2<float> &INIT_POS);
 
 	// 更新処理
 	void Update(const vector<vector<int>> mapData);
 
 	// 描画処理
-	void Draw(LightManager& LigManager);
+	void Draw(LightManager &LigManager);
 
 	// マップチップとの当たり判定
-	void CheckHit(const vector<vector<int>> mapData, vector<Bubble>& bubble, vector<DossunBlock>& dossun);
+	void CheckHit(const vector<vector<int>> mapData, vector<Bubble> &bubble, vector<DossunBlock> &dossun);
 
 	// 方向ごとのマップチップとの当たり判定関数
 	void HitMapChipTop();
@@ -171,12 +171,12 @@ private:
 	bool doorMoveUpDownFlag;
 	bool doorMoveDownFlag;
 	//ストレッチの値を計算
-	void CalculateStretch(const Vec2<float>& Move);
+	void CalculateStretch(const Vec2<float> &Move);
 	//ストレッチ値更新
 	void UpdateStretch();
 	//画像サイズからプレイヤーサイズ取得
 	Vec2<float> GetPlayerGraphSize();
 
 	//プレイヤーの手の画像ハンドル取得
-	int GetHandGraph(const DRAW_DIR& Dir);
+	int GetHandGraph(const DRAW_DIR &Dir);
 };
