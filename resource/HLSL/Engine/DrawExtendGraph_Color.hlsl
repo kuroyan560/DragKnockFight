7c02818427e36af401af10669571bcdcf1bcbcbb
hassy560 <k020g1167@g.neec.ac.jp>
2022-03-23 13:08:00 +0900

    DrawFunc_Color送り忘れ

---
+++ resource/HLSL/Engine/DrawExtendGraph_Color.hlsl
@@ -0,0 +1,76 @@
+cbuffer cbuff0 : register(b0)
+{
+    matrix parallelProjMat; //平行投影行列
+};
+
+struct VSOutput
+{
+    float4 leftUpPos : POSITION_L_U;
+    float4 rightBottomPos : POSITION_R_B;
+    float4 paintColor : PAINT_COLOR;
+};
+
+VSOutput VSmain(VSOutput input)
+{
+    return input;
+}
+
+struct GSOutput
+{
+    float4 pos : SV_POSITION;
+    float2 uv : TEXCOORD;
+    float4 paintColor : PAINT_COLOR;
+};
+
+
+[maxvertexcount(4)]
+void GSmain(
+	point VSOutput input[1],
+	inout TriangleStream<GSOutput> output
+)
+{
+    float width = input[0].rightBottomPos.x - input[0].leftUpPos.x;
+    
+    GSOutput element;
+    element.paintColor = input[0].paintColor;
+        
+    //左下
+    element.pos = input[0].rightBottomPos;
+    element.pos.x -= width;
+    element.pos = mul(parallelProjMat, element.pos);
+    element.uv = float2(0.0f, 1.0f);
+    output.Append(element);
+    
+    //左上
+    element.pos = input[0].leftUpPos;
+    element.pos = mul(parallelProjMat, element.pos);
+    element.uv = float2(0.0f, 0.0f);
+    output.Append(element);
+    
+     //右下
+    element.pos = input[0].rightBottomPos;
+    element.pos = mul(parallelProjMat, element.pos);
+    element.uv = float2(1.0f, 1.0f);
+    output.Append(element);
+    
+    //右上
+    element.pos = input[0].leftUpPos;
+    element.pos.x += width;
+    element.pos = mul(parallelProjMat, element.pos);
+    element.uv = float2(1.0f, 0.0f);
+    output.Append(element);
+}
+
+Texture2D<float4> tex : register(t0);
+SamplerState smp : register(s0);
+
+float4 PSmain(GSOutput input) : SV_TARGET
+{
+    float4 texCol = tex.Sample(smp, input.uv);
+    return float4(input.paintColor.xyz, texCol.w * input.paintColor.w);
+}
+
+float4 main(float4 pos : POSITION) : SV_POSITION
+{
+    return pos;
+}
---
+++ resource/HLSL/Engine/DrawRotaGraph_Color.hlsl
@@ -0,0 +1,102 @@
+cbuffer cbuff0 : register(b0)
+{
+    matrix parallelProjMat; //平行投影行列
+};
+
+struct VSOutput
+{
+    float4 center : CENTER;
+    float2 extRate : EXT_RATE;
+    float radian : RADIAN;
+    float2 rotaCenterUV : ROTA_CENTER_UV;
+    float4 paintColor : PAINT_COLOR;
+};
+
+VSOutput VSmain(VSOutput input)
+{
+    return input;
+}
+
+struct GSOutput
+{
+    float4 pos : SV_POSITION;
+    float2 uv : TEXCOORD;
+    float4 paintColor : PAINT_COLOR;
+};
+
+Texture2D<float4> tex : register(t0);
+SamplerState smp : register(s0);
+
+float2 RotateFloat2(float2 Pos,float Radian)
+{
+    float2 result;
+    result.x = Pos.x * cos(Radian) - Pos.y * sin(Radian);
+    result.y = Pos.y * cos(Radian) + Pos.x * sin(Radian);
+    return result;
+}
+
+[maxvertexcount(4)]
+void GSmain(
+	point VSOutput input[1],
+	inout TriangleStream<GSOutput> output
+)
+{
+    uint2 texSize;
+    tex.GetDimensions(texSize.x, texSize.y);
+    
+    float width_h = texSize.x * input[0].extRate.x / 2.0f;
+    float height_h = texSize.y * input[0].extRate.y / 2.0f;
+    
+    float2 rotateCenter = input[0].center.xy;
+    rotateCenter += texSize.xy * input[0].extRate * (input[0].rotaCenterUV - float2(0.5f, 0.5f));
+    
+    GSOutput element;
+    element.paintColor = input[0].paintColor;
+        
+    //左下
+    element.pos = input[0].center;
+    element.pos.x -= width_h;
+    element.pos.y += height_h;
+    element.pos.xy = rotateCenter + RotateFloat2(element.pos.xy - rotateCenter, input[0].radian);
+    element.pos = mul(parallelProjMat, element.pos);
+    element.uv = float2(0.0f, 1.0f);
+    output.Append(element);
+    
+    //左上
+    element.pos = input[0].center;
+    element.pos.x -= width_h;
+    element.pos.y -= height_h;
+    element.pos.xy = rotateCenter + RotateFloat2(element.pos.xy - rotateCenter, input[0].radian);
+    element.pos = mul(parallelProjMat, element.pos);
+    element.uv = float2(0.0f, 0.0f);
+    output.Append(element);
+    
+     //右下
+    element.pos = input[0].center;
+    element.pos.x += width_h;
+    element.pos.y += height_h;
+    element.pos.xy = rotateCenter + RotateFloat2(element.pos.xy - rotateCenter, input[0].radian);
+    element.pos = mul(parallelProjMat, element.pos);
+    element.uv = float2(1.0f, 1.0f);
+    output.Append(element);
+    
+    //右上
+    element.pos = input[0].center;
+    element.pos.x += width_h;
+    element.pos.y -= height_h;
+    element.pos.xy = rotateCenter + RotateFloat2(element.pos.xy - rotateCenter, input[0].radian);
+    element.pos = mul(parallelProjMat, element.pos);
+    element.uv = float2(1.0f, 0.0f);
+    output.Append(element);
+}
+
+float4 PSmain(GSOutput input) : SV_TARGET
+{
+    float4 texCol = tex.Sample(smp, input.uv);
+    return float4(input.paintColor.xyz, texCol.w * input.paintColor.w);
+}
+
+float4 main(float4 pos : POSITION) : SV_POSITION
+{
+    return pos;
+}
