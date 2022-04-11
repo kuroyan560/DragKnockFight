b28a7cecfe742523a1eb5b67c52623158c8ea581
フナクラベ <k020g1164@g.neec.ac.jp>
2022-04-11 19:37:29 +0900

    とりあえず上部分だけ バックアップのためにコミット！

--- src/ResultScene.cpp
+++ src/ResultScene.cpp
@@ -2,6 +2,8 @@
 #include "IntoTheAbyss/TexHandleMgr.h"
 #include "DrawFunc.h"
 #include "WinApp.h"
+#include "KuroMath.h"
+#include "UsersInput.h"
 
 ResultScene::ResultScene()
 {
@@ -9,19 +11,96 @@ ResultScene::ResultScene()
 
 void ResultScene::OnInitialize()
 {
-	backGroundHandle = TexHandleMgr::LoadGraph("resource/ChainCombat/title_scene/back.png");
+	backGroundHandle = TexHandleMgr::LoadGraph("resource/ChainCombat/title_scene/star.png");
+	winnerFrameHandle = TexHandleMgr::LoadGraph("resource/ChainCombat/result_scene/winnerFrame.png");
+	resultHandle = TexHandleMgr::LoadGraph("resource/ChainCombat/result_scene/result.png");
+	breakEnemyHandle = TexHandleMgr::LoadGraph("resource/ChainCombat/UI/break_enemy.png");
+	breakPlayerHandle = TexHandleMgr::LoadGraph("resource/ChainCombat/UI/break_player.png");
+	scoreHandle = TexHandleMgr::LoadGraph("resource/ChainCombat/UI/score.png");
+
+	resultUITimer = 0;
+	breakEnemyUITimer = 0;
+	breakPlayerUITimer = 0;
+	delayTimer = 0;
+	resultEasingAmount = 0;
+	breakEnemyEasingAmount = 0;
+	breakPlayerEasingAmount = 0;
+
 }
 
 void ResultScene::OnUpdate()
 {
+
+	Vec2<float> windowSize = { (float)WinApp::Instance()->GetWinSize().x, (float)WinApp::Instance()->GetWinSize().y };
+
+	// 遅延タイマーが既定値以下だったらインクリメントする。
+	if (delayTimer < DELAY_TIMER) ++delayTimer;
+
+	// 遅延が発生していなかったら。
+	if (DELAY_TIMER <= delayTimer) {
+
+		// [RESULT]の画像 [RESULT]のタイマーが既定値以下だったら。
+		if (resultUITimer < RESULT_UI_TIMER) {
+			++resultUITimer;
+			// タイマーが規定値に達したら。
+			if (RESULT_UI_TIMER <= resultUITimer) {
+				delayTimer = 0;
+			}
+			else {
+				delayTimer = DELAY_TIMER;
+			}
+		}
+		// [BREAK]敵の画像 タイマーが規定値以下だったら。
+		if (breakEnemyUITimer < BREAK_ENEMY_UI_TIMER && RESULT_UI_TIMER <= resultUITimer) {
+			++breakEnemyUITimer;
+			// タイマーが規定値に達したら。
+			if (BREAK_ENEMY_UI_TIMER <= breakEnemyUITimer) {
+				delayTimer = 0;
+			}
+			else {
+				delayTimer = DELAY_TIMER;
+			}
+		}
+		// [BREAK}プレイヤーの画像 タイマーが規定値以下だったら。
+		if (breakPlayerUITimer < BREAK_PLAYER_UI_TIMER && BREAK_ENEMY_UI_TIMER <= breakEnemyUITimer) {
+			++breakPlayerUITimer;
+			// タイマーが規定値に達したら。
+			if (BREAK_PLAYER_UI_TIMER <= breakPlayerUITimer) {
+				delayTimer = 0;
+			}
+			else {
+				delayTimer = DELAY_TIMER;
+			}
+		}
+	}
+
+	// イージング量を更新。
+	resultEasingAmount = KuroMath::Ease(Out, Cubic, (float)resultUITimer / RESULT_UI_TIMER, 0.0f, 1.0f);
+	breakEnemyEasingAmount = KuroMath::Ease(Out, Cubic, (float)breakEnemyUITimer / BREAK_ENEMY_UI_TIMER, 0.0f, 1.0f);
+	breakPlayerEasingAmount = KuroMath::Ease(Out, Cubic, (float)breakPlayerUITimer / BREAK_PLAYER_UI_TIMER, 0.0f, 1.0f);
+
+	if (UsersInput::Instance()->Input(DIK_R)) {
+
+		OnInitialize();
+
+	}
+
 }
 
 void ResultScene::OnDraw()
 {
+
 	KuroEngine::Instance().Graphics().SetRenderTargets({ D3D12App::Instance()->GetBackBuffRenderTarget() });
 
 	Vec2<float> windowSize = { (float)WinApp::Instance()->GetWinSize().x, (float)WinApp::Instance()->GetWinSize().y };
-	DrawFunc::DrawExtendGraph2D(Vec2<float>(0, 0), windowSize, TexHandleMgr::GetTexBuffer(backGroundHandle));
+	DrawFunc::DrawBox2D(Vec2<float>(0, 0), windowSize, Color(0, 0, 0, 255), DXGI_FORMAT_R8G8B8A8_UNORM);
+	DrawFunc::DrawGraph(Vec2<float>(0, 0), TexHandleMgr::GetTexBuffer(winnerFrameHandle), AlphaBlendMode_Trans);
+	float easingPosX = resultEasingAmount * (RESULT_POS.x - windowSize.x);
+	DrawFunc::DrawGraph(Vec2<float>(windowSize.x + easingPosX, RESULT_POS.y), TexHandleMgr::GetTexBuffer(resultHandle), AlphaBlendMode_Trans);
+	easingPosX = breakEnemyEasingAmount * (BREAK_ENEMY_POS.x - windowSize.x);
+	DrawFunc::DrawGraph(Vec2<float>(windowSize.x + easingPosX, BREAK_ENEMY_POS.y), TexHandleMgr::GetTexBuffer(breakEnemyHandle), AlphaBlendMode_Trans);
+	easingPosX = breakPlayerEasingAmount * (BREAK_PLAYER_POS.x - windowSize.x);
+	DrawFunc::DrawGraph(Vec2<float>(windowSize.x + easingPosX, BREAK_PLAYER_POS.y), TexHandleMgr::GetTexBuffer(breakPlayerHandle), AlphaBlendMode_Trans);
 
 }
 
--- src/ResultScene.h
+++ src/ResultScene.h
@@ -1,6 +1,8 @@
 #pragma once
 #include"KuroEngine.h"
 #include"LightManager.h"
+#include"Vec.h"
+#include"WinApp.h"
 
 #include"IntoTheAbyss/Game.h"
 #include"SceneCange.h"
@@ -8,7 +10,40 @@
 class ResultScene : public BaseScene
 {
 
-	int backGroundHandle;
+	int backGroundHandle;		// 背景(仮)の画像ハンドル
+	int winnerFrameHandle;		// 勝者のフレームの画像ハンドル
+	int resultHandle;			// リザルトの画像ハンドル
+	int breakEnemyHandle;		// BREAKの画像ハンドル 敵
+	int breakPlayerHandle;		// BREAKの画像ハンドル プレイヤー
+	int scoreHandle;			// SCOREの画像ハンドル
+
+	// 各タイマー
+	int resultUITimer;			// リザルトの画像のイージングに使用するタイマー
+	int breakEnemyUITimer;		// BREAKの画像ハンドル敵に使用するタイマー
+	int breakPlayerUITimer;		// BREAKの画像ハンドルプレイヤーに使用するタイマー
+	int delayTimer;				// 各イージングの間の遅延タイマー
+
+	// 各イージング量
+	float resultEasingAmount;		// リザルトの画像のイージング量
+	float breakEnemyEasingAmount;	// BREAKの画像のイージング量
+	float breakPlayerEasingAmount;	// BREAKの画像のイージング量
+
+	// 各タイマーのデフォルト値
+	const int RESULT_UI_TIMER = 20;
+	const int BREAK_ENEMY_UI_TIMER = 20;
+	const int BREAK_PLAYER_UI_TIMER = 20;
+	const int DELAY_TIMER = 30;
+
+
+public:
+
+	const Vec2<int> WINDOW_CENTER = WinApp::Instance()->GetWinCenter();
+
+	// イージング結果の座標
+	const Vec2<float> RESULT_POS = { (float)WINDOW_CENTER.x - 80.0f, 30.0f };
+	const Vec2<float> BREAK_ENEMY_POS = { (float)WINDOW_CENTER.x + 10.0f, 150.0f };
+	const Vec2<float> BREAK_PLAYER_POS = { (float)WINDOW_CENTER.x + 90.0f, 250.0f };
+
 
 public:
 	ResultScene();
