2247f0bfb0fb26aef965e01148cd12a10981eeb1
hassy560 <k020g1167@g.neec.ac.jp>
2022-04-19 15:20:41 +0900

    踏ん張りアニメーション（仮）

--- src/IntoTheAbyss/Player.cpp
+++ src/IntoTheAbyss/Player.cpp
@@ -136,12 +136,6 @@ void Player::OnUpdate(const vector<vector<int>>& MapData)
 	// 一個上のマップチップがブロックで、X軸方向の移動量が一定以上だったらパーティクルを生成する。
 	//if (MapData[mapY][mapX] > 0 && MapData[mapY][mapX] < 10 && fabs(vel.x) >= 10.0f)ParticleMgr::Instance()->Generate(Vec2<float>(pos.x, pos.y - GetPlayerGraphSize().y), Vec2<float>(0, -1), HIT_MAP);
 
-	//ストレッチ更新
-	UpdateStretch();
-
-	//アニメーション更新
-	anim.Update();
-
 	// 振り回しのクールタイムを更新
 	if (0 < swingCoolTime) --swingCoolTime;
 
@@ -166,14 +160,23 @@ void Player::OnUpdateNoRelatedSwing()
 
 		}
 	}
+
+	//ストレッチ更新
+	UpdateStretch();
+
+	//アニメーション更新
+	anim.Update();
 }
 
 void Player::OnDraw()
 {
 	//if (vel.y < 0)playerDir = BACK;
-	if (vel.y < 0)anim.ChangeAnim(DEFAULT_BACK);
-	//if (0 < vel.y)playerDir = FRONT;
-	if (0 < vel.y)anim.ChangeAnim(DEFAULT_FRONT);
+	if (!isHold)
+	{
+		if (vel.y < 0)anim.ChangeAnim(DEFAULT_BACK);
+		//if (0 < vel.y)playerDir = FRONT;
+		if (0 < vel.y)anim.ChangeAnim(DEFAULT_FRONT);
+	}
 
 	/*===== 描画処理 =====*/
 
@@ -199,8 +202,10 @@ void Player::OnDraw()
 	//胴体
 	auto bodyTex = TexHandleMgr::GetTexBuffer(anim.GetGraphHandle());
 	const Vec2<float> expRateBody = ((GetPlayerGraphSize() - stretch_LU + stretch_RB) / GetPlayerGraphSize());
+	bool mirorX = 0 < vel.x || (isHold && (partner.lock()->pos - pos).x < 0);
+	if (GetWhichTeam() == RIGHT_TEAM)mirorX = !mirorX;
 	DrawFunc_FillTex::DrawRotaGraph2D(drawPos, expRateBody * ScrollMgr::Instance()->zoom * EXT_RATE * stagingDevice.GetExtRate() * size,
-		0.0f, bodyTex, CRASH_TEX, stagingDevice.GetFlashAlpha(), { 0.5f,0.5f }, { GetWhichTeam() == RIGHT_TEAM,false });
+		0.0f, bodyTex, CRASH_TEX, stagingDevice.GetFlashAlpha(), { 0.5f,0.5f }, { mirorX,false });
 }
 
 void Player::OnDrawUI()
@@ -342,6 +347,8 @@ void Player::Input(const vector<vector<int>>& MapData)
 	// [LTを押されたら] [握力が残っていたら] [握力を使い切ってから回復している状態じゃなかったら]
 	if (UsersInput::Instance()->ControllerInput(controllerIdx, XBOX_BUTTON::LT) && 0 < gripPowerTimer && !isGripPowerEmpty) {
 
+		anim.ChangeAnim(HOLD);
+
 		// 紐つかみ状態(踏ん張り状態)にする。
 		isHold = true;
 
@@ -360,6 +367,7 @@ void Player::Input(const vector<vector<int>>& MapData)
 
 	}
 	else {
+		if (anim.GetNowAnim() == HOLD)anim.ChangeAnim(DEFAULT_FRONT);
 
 		// 紐つかみ状態(踏ん張り状態)を解除する。
 		isHold = false;
--- src/IntoTheAbyss/PlayerAnimation.cpp
+++ src/IntoTheAbyss/PlayerAnimation.cpp
@@ -9,30 +9,41 @@ PlayerAnimation::PlayerAnimation(const PLAYABLE_CHARACTER_NAME& CharacterName)
 		"luna",
 		"lacy"
 	};
+	static const std::string DIR = "resource/ChainCombat/player/";
 
 	static const int DEFAULT_FRONT_NUM = 1;
-	animations[DEFAULT_FRONT].graph.resize(1);
+	animations[DEFAULT_FRONT].graph.resize(DEFAULT_FRONT_NUM);
 	//TexHandleMgr::LoadDivGraph("resource/IntoTheAbyss/Player/on_ground_wait.png", ON_GROUND_WAIT_NUM, { ON_GROUND_WAIT_NUM,1 }, animations[ON_GROUND_WAIT].graph.data());
-	animations[DEFAULT_FRONT].graph[0] = TexHandleMgr::LoadGraph("resource/ChainCombat/player/" + NAME_DIR[CharacterName] + "/player.png");
+	animations[DEFAULT_FRONT].graph[0] = TexHandleMgr::LoadGraph(DIR + NAME_DIR[CharacterName] + "/player.png");
 	animations[DEFAULT_FRONT].interval = 10;
 	animations[DEFAULT_FRONT].loop = true;
 	animations[DEFAULT_FRONT].size = { 123,123 };
 	animations[DEFAULT_FRONT].handCenterOffset = { 0.0f,0.0f };
 
 	static const int DEFAULT_BACK_NUM = 1;
-	animations[DEFAULT_BACK].graph.resize(1);
-	animations[DEFAULT_BACK].graph[0] = TexHandleMgr::LoadGraph("resource/ChainCombat/player/" + NAME_DIR[CharacterName] + "/player_back.png");
+	animations[DEFAULT_BACK].graph.resize(DEFAULT_BACK_NUM);
+	animations[DEFAULT_BACK].graph[0] = TexHandleMgr::LoadGraph(DIR + NAME_DIR[CharacterName] + "/player_back.png");
 	animations[DEFAULT_BACK].interval = 10;
 	animations[DEFAULT_BACK].loop = true;
 	animations[DEFAULT_BACK].size = { 117,96 };
 	animations[DEFAULT_BACK].handCenterOffset = { 0.0f,0.0f };
+
+	static const int HOLD_NUM = 3;
+	animations[HOLD].graph.resize(HOLD_NUM);
+	TexHandleMgr::LoadDivGraph(DIR + NAME_DIR[CharacterName] + "/hold.png", HOLD_NUM, { HOLD_NUM,1 }, animations[HOLD].graph.data());
+	animations[HOLD].interval = 6;
+	animations[HOLD].loop = true;
+	animations[HOLD].size = { 230,220 };
+	animations[HOLD].handCenterOffset = { 0.0f,0.0f };
+
 }
 
+#include"SlowMgr.h"
 void PlayerAnimation::Update()
 {
 	auto& anim = animations[status];
-	timer++;
-	if (anim.interval == timer)
+	timer += SlowMgr::Instance()->slowAmount;
+	if (anim.interval <= timer)
 	{
 		idx++;
 		if (anim.graph.size() == idx)
