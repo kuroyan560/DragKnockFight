0426ee0df777f5560069ef501f0b3baa787fb5f0
フナクラベ <k020g1164@g.neec.ac.jp>
2022-04-28 14:46:44 +0900

    画像をマージするためにコミット。

--- src/StageSelectScene.cpp
+++ src/StageSelectScene.cpp
@@ -3,11 +3,17 @@
 #include"IntoTheAbyss/StageMgr.h"
 #include"IntoTheAbyss/CharacterManager.h"
 #include"Vec.h"
+#include"KuroMath.h"
+#include"DrawFunc_Color.h"
+#include"DrawFunc.h"
 
 StageSelectScene::StageSelectScene()
 {
 	changeScene = std::make_shared<SceneCange>();
 	stageNum = 0;
+
+	multiHandle = TexHandleMgr::LoadGraph("resource/ChainCombat/select_scene/players/multiPlay.png");
+	singleHandle = TexHandleMgr::LoadGraph("resource/ChainCombat/select_scene/players/singlePlay.png");
 }
 
 void StageSelectScene::OnInitialize()
@@ -17,6 +23,9 @@ void StageSelectScene::OnInitialize()
 	isSelectMultiSingle = true;
 	isMulti = false;
 	isSingle = true;	// マルチ、シングル選択画面において最初にシングルプレイの画像を表示するためにtrueにする。
+	easingTimer = 0;
+	singleTexturePos = CURRENT_DEF_POS;
+	multiTexturePos = BEHIND_DEF_POS;
 }
 
 void StageSelectScene::OnUpdate()
@@ -31,14 +40,8 @@ void StageSelectScene::OnUpdate()
 	// マルチ、シングル選択画面を表示する。
 	if (isSelectMultiSingle) {
 
-		// 左スティックの入力を保存する。
-		Vec2<float> inputLeftVec = UsersInput::Instance()->GetLeftStickVecFuna(0);	// 1Pからの入力を受け付ける。
-		inputLeftVec /= {32768.0f, 32768.0f};
-
-		// 入力のデッドライン
-
-		// 右側に入力されたら。
-		if(0.5f < inputLeftVec.x)
+		// マルチ、シングルの選択画面での更新処理を行う。
+		SelectMultiSingleUpdate();
 
 	}
 	// マルチ、シングル選択が画面終わり。フラグによって処理を変える。
@@ -108,6 +111,33 @@ void StageSelectScene::OnDraw()
 {
 	KuroEngine::Instance().Graphics().SetRenderTargets({ D3D12App::Instance()->GetBackBuffRenderTarget() });
 	KuroEngine::Instance().Graphics().SetRenderTargets({ D3D12App::Instance()->GetBackBuffRenderTarget() });
+
+	// マルチシングル選択画面だったら
+	if (isSelectMultiSingle) {
+
+		const Vec2<float> PLAY_STR_POS = { 1700,700 };							// 何人プレイかを描画する座標。
+
+			// マルチプレイを選択中だったら
+		if (isMulti) {
+
+			// シングルの画像を表示
+			DrawFunc::DrawGraph(singleTexturePos, TexHandleMgr::GetTexBuffer(singleHandle));
+			// マルチの画像を表示
+			DrawFunc::DrawGraph(multiTexturePos, TexHandleMgr::GetTexBuffer(multiHandle));
+
+		}
+		else if (isSingle) {
+
+			// マルチの画像を表示
+			DrawFunc::DrawGraph(multiTexturePos, TexHandleMgr::GetTexBuffer(multiHandle));
+			// シングルの画像を表示
+			DrawFunc::DrawGraph(singleTexturePos, TexHandleMgr::GetTexBuffer(singleHandle));
+
+		}
+
+
+
+	}
 }
 
 void StageSelectScene::OnImguiDebug()
@@ -131,4 +161,128 @@ void StageSelectScene::OnImguiDebug()
 
 void StageSelectScene::OnFinalize()
 {
+}
+
+void StageSelectScene::SelectMultiSingleUpdate()
+{
+
+	/*===== マルチ、シングル選択画面での更新処理 =====*/
+
+
+	// 左スティックの入力を保存する。
+	Vec2<float> inputLeftVec = UsersInput::Instance()->GetLeftStickVecFuna(0);	// 1Pからの入力を受け付ける。
+	inputLeftVec /= {32768.0f, 32768.0f};
+
+	// どちらかに入力されたら。
+	bool isEndEasing = easingTimer == 0;	// イージングが終わっているか。
+	if (0.5f < fabs(inputLeftVec.x) && isEndEasing) {
+
+		// 今のフラグが一人プレイ描画中だったら。
+		if (isSingle) {
+
+			// マルチプレイにする。
+			isMulti = true;
+			isSingle = false;
+
+		}
+		// 今のフラグがマルチプレイ描画中だったら。
+		else {
+
+			// シングルプレイにする。
+			isMulti = false;
+			isSingle = true;
+
+		}
+
+		// イージングタイマーを進める。こうすることで自動的にイージングが始まる。
+		easingTimer += ADD_EASING_TIMER;
+
+	}
+
+	// イージングタイマーが起動していたらイージングを進める。
+	if (easingTimer != 0) {
+
+		// 選択中だったのがシングルプレイだったら。
+		if (isSingle) {
+
+			// イージングのタイマーの値によって今が前半か後半かを取得する。
+			bool isFirstHalf = easingTimer < 0.5f;
+
+			// 前半だったら。
+			if (isFirstHalf) {
+
+				float easingRate = easingTimer / 0.5f;
+
+				float easingAmount = KuroMath::Ease(Out, Sine, easingRate, 0.0f, 1.0f);
+
+				// シングルプレイを背面から正面に持ってくる。
+				singleTexturePos = BEHIND_DEF_POS + (CURRENT_INTERIM_POS - BEHIND_DEF_POS) * easingAmount;
+
+				// マルチプレイを全面から背面に持ってくる。
+				multiTexturePos = CURRENT_DEF_POS + (BEHIND_INTERIM_POS - CURRENT_DEF_POS) * easingAmount;
+
+			}
+			// 後半だったら。
+			else {
+
+				float easingRate = (easingTimer - 0.5f) / 0.5f;
+
+				float easingAmount = KuroMath::Ease(In, Sine, easingRate, 0.0f, 1.0f);
+
+				// シングルプレイを背面から正面に持ってくる。
+				singleTexturePos = CURRENT_INTERIM_POS + (CURRENT_DEF_POS - CURRENT_INTERIM_POS) * easingAmount;
+
+				// マルチプレイを全面から背面に持ってくる。
+				multiTexturePos = BEHIND_INTERIM_POS + (BEHIND_DEF_POS - BEHIND_INTERIM_POS) * easingAmount;
+
+			}
+
+		}
+		// 選択中だったのがマルチプレイだったら。
+		else {
+
+			// イージングのタイマーの値によって今が前半か後半かを取得する。
+			bool isFirstHalf = easingTimer < 0.5f;
+
+			// 前半だったら。
+			if (isFirstHalf) {
+
+				float easingRate = easingTimer / 0.5f;
+
+				float easingAmount = KuroMath::Ease(Out, Sine, easingRate, 0.0f, 1.0f);
+
+				// シングルプレイを前面から背面に持っていく。
+				singleTexturePos = CURRENT_DEF_POS + (BEHIND_INTERIM_POS - CURRENT_DEF_POS) * easingAmount;
+
+				// マルチプレイを背面から前面に持っていく。
+				multiTexturePos = BEHIND_DEF_POS + (CURRENT_INTERIM_POS - BEHIND_DEF_POS) * easingAmount;
+
+			}
+			// 後半だったら。
+			else {
+
+				float easingRate = (easingTimer - 0.5f) / 0.5f;
+
+				float easingAmount = KuroMath::Ease(In, Sine, easingRate, 0.0f, 1.0f);
+
+				// シングルプレイを前面から背面に持っていく。
+				singleTexturePos = BEHIND_INTERIM_POS + (BEHIND_DEF_POS - BEHIND_INTERIM_POS) * easingAmount;
+
+				// マルチプレイを全面から背面に持ってくる。
+				multiTexturePos = CURRENT_INTERIM_POS + (CURRENT_DEF_POS - CURRENT_INTERIM_POS) * easingAmount;
+
+			}
+
+		}
+
+		// イージングタイマーを更新。
+		easingTimer += ADD_EASING_TIMER;
+		if (1.0f <= easingTimer) {
+
+			easingTimer = 0;
+
+		}
+
+	}
+
 }
--- src/StageSelectScene.h
+++ src/StageSelectScene.h
@@ -10,12 +10,26 @@ class StageSelectScene : public BaseScene
 	//キャラクター選択中フラグ
 	bool charactersSelect;
 
-	// マルチシングル選択画面かフラグ
-	bool isSelectMultiSingle;
 
-	// マルチかシングルか
-	bool isMulti;
-	bool isSingle;
+	/*-- マルチ、シングルを選択する画面で使用する変数 --*/
+
+	bool isSelectMultiSingle;		// マルチシングル選択画面かフラグ
+	bool isMulti;					// マルチプレイを選択
+	bool isSingle;					// シングルプレイを選択
+	float easingTimer;				// イージングに使用するタイマー
+	Vec2<float> multiTexturePos;	// マルチプレイの画像の左上の座標
+	Vec2<float> singleTexturePos;	// シングルプレイの画像の左上の座標
+	int multiHandle;
+	int singleHandle;
+
+	const Vec2<float> SINGLE_MULTI_TEX_SIZE = { 1055.0f,640.0f };			// マルチプレイ、シングルプレイの説明画像のサイズ
+	const Vec2<float> CURRENT_DEF_POS = { 0.0f,0.0f };						// 選択中の画像の左上の位置。
+	const Vec2<float> CURRENT_INTERIM_POS = SINGLE_MULTI_TEX_SIZE / 2.0f;	// 選択した画像が正面に出る時に経由する座標。
+	const Vec2<float> BEHIND_INTERIM_POS = -SINGLE_MULTI_TEX_SIZE / 2.0f;	// 選択されなかった画像が背面に戻る時に経由する座標。
+	const Vec2<float> BEHIND_DEF_POS = SINGLE_MULTI_TEX_SIZE / 10.0f;		// 選択されなかった画像の左上の座標。
+	const Vec2<float> PLAY_STR_POS = { 1700,700 };
+	const float ADD_EASING_TIMER = 0.03f;	// イージングん加算量
+
 
 public:
 	StageSelectScene();
@@ -24,6 +38,9 @@ public:
 	void OnDraw()override;
 	void OnImguiDebug()override;
 	void OnFinalize()override;
+
+	// マルチかシングルかを選択する更新処理。
+	void SelectMultiSingleUpdate();
 
 	std::shared_ptr<SceneCange>changeScene;
 
