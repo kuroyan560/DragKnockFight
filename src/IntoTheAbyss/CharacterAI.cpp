95f9cca4b8a0e3e59b7e8829c0af3e6c6af88d46
LC1A_07_オオイシカズノリ <k020g1151@g.neec.ac.jp>
2022-05-23 01:13:49 +0900

    ダッシュの戦略を削除

--- src/IntoTheAbyss/CharacterAI.cpp
+++ src/IntoTheAbyss/CharacterAI.cpp
@@ -11,7 +11,7 @@ CharacterAI::CharacterAI()
 	useAiFlag = false;
 
 	std::vector<BehaviorGraphData>data;
-	data.resize(5);
+	data.resize(STRATEGY_MAX);
 
 	std::string pass = "resource/ChainCombat/boss/behaviorPrediction/";
 	std::string pass2 = "resource/ChainCombat/boss/0/icon/";
@@ -19,12 +19,12 @@ CharacterAI::CharacterAI()
 	int roopNum = 10;
 	//ƒ_ƒbƒVƒ…
 	{
-		const int GRAPH_NUM = 3;
-		data[STRATEGY_SWING_DASH].handle.graph.resize(GRAPH_NUM);
-		data[STRATEGY_SWING_DASH].handle.interval = roopNum;
-		data[STRATEGY_SWING_DASH].handle.loop = true;
-		data[STRATEGY_SWING_DASH].cautionFlag = false;
-		TexHandleMgr::LoadDivGraph(pass + "dash.png", GRAPH_NUM, { GRAPH_NUM,1 }, data[dataArrayNum].handle.graph.data());
+		//const int GRAPH_NUM = 3;
+		//data[STRATEGY_SWING_DASH].handle.graph.resize(GRAPH_NUM);
+		//data[STRATEGY_SWING_DASH].handle.interval = roopNum;
+		//data[STRATEGY_SWING_DASH].handle.loop = true;
+		//data[STRATEGY_SWING_DASH].cautionFlag = false;
+		//TexHandleMgr::LoadDivGraph(pass + "dash.png", GRAPH_NUM, { GRAPH_NUM,1 }, data[dataArrayNum].handle.graph.data());
 	}
 
 	++dataArrayNum;
@@ -84,12 +84,12 @@ void CharacterAI::Init()
 		strategyArray[STRATEGY_SWING_3TIMES_CLOCKWISE] = std::make_unique<SwingClockWiseThreeTimes>();
 		strategyArray[STRATEGY_SWING_COUNTERCLOCKWISE] = std::make_unique<SwingCounterClockWise>();
 		strategyArray[STRATEGY_SWING_CLOCKWISE] = std::make_unique<SwingClockWise>();
-		strategyArray[STRATEGY_SWING_DASH] = std::make_unique<Dash>();
+		//strategyArray[STRATEGY_SWING_DASH] = std::make_unique<Dash>();
 		//í—ª‘w‚Ì¶¬--------------------------
 	}
 	initFlag = true;
 	useAiFlag = true;
-	strategyOfChoice = STRATEGY_SWING_DASH;
+	strategyOfChoice = STRATEGY_SWING_CLOCKWISE;
 	startFlag = false;
 	CharacterAIData::Instance()->dashTimer = 0;
 	startDashFlag = false;
