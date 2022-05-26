45e353b970435b1674cc20ae2fda983300b2ea6a
LC1A_07_オオイシカズノリ <k020g1151@g.neec.ac.jp>
2022-05-26 20:58:46 +0900

    BlockCountの表示ずれを修正

--- src/IntoTheAbyss/BlockCount.cpp
+++ src/IntoTheAbyss/BlockCount.cpp
@@ -23,41 +23,44 @@ void BlockCount::Update()
 	countNowBlockNum = StageMgr::Instance()->GetAllLocalWallBlocksNum();
 	nowNumber = CountNumber(countNowBlockNum);
 
-	basePos = { 1280.0f / 2.0f - 200.0f,90.0f };
+	basePos = { 1280.0f / 2.0f,90.0f };
 }
 
 void BlockCount::Draw()
 {
-	Vec2<float>texSize(48.0f, 44.0f);
+	Vec2<float>texSize(30.0f, 44.0f);
 	Vec2<float>centralPos;
 	int offset = 0;
 	float size = 0.4f;
+
+	int num = KuroFunc::GetDigit(countAllBlockNum);
+
+	Vec2<float>drawPos = basePos - Vec2<float>(10.0f, 0.0f);
+	for (int i = 0; i < num; ++i)
+	{
+		drawPos.x -= texSize.x;
+	}
 	//����
 	for (int i = 0; i < nowNumber.size(); i++)
 	{
-		offset = i;
-
-		centralPos = { 65.0f + basePos.x + (i * texSize.x - 15.0f * i), basePos.y };
-		DrawFunc::DrawRotaGraph2D(centralPos, Vec2<float>(size, size), 0.0f, TexHandleMgr::GetTexBuffer(number[nowNumber[i]]));
+		drawPos.x += texSize.x;
+		DrawFunc::DrawRotaGraph2D(drawPos - Vec2<float>(25.0f, 0.0f), Vec2<float>(size, size), 0.0f, TexHandleMgr::GetTexBuffer(number[nowNumber[i]]));
 	}
 
-	++offset;
-	centralPos = { basePos.x + offset * texSize.x,basePos.y };
-	DrawFunc::DrawRotaGraph2D(centralPos, Vec2<float>(size, size), 0.0f, TexHandleMgr::GetTexBuffer(number[10]));
-	++offset;
-
+	DrawFunc::DrawRotaGraph2D(basePos + Vec2<float>(-8.0f, 0.0f), Vec2<float>(size, size), 0.0f, TexHandleMgr::GetTexBuffer(number[10]));
+	drawPos = basePos + Vec2<float>(10.0f, 0.0f);
 	//�ő�
 	for (int i = 0; i < maxNumber.size(); i++)
 	{
-		centralPos = { basePos.x - 17.0f + (offset + i) * texSize.x - 15.0f * i, basePos.y };
-		DrawFunc::DrawRotaGraph2D(centralPos, Vec2<float>(size, size), 0.0f, TexHandleMgr::GetTexBuffer(number[maxNumber[i]]));
+		drawPos.x += texSize.x;
+		DrawFunc::DrawRotaGraph2D(drawPos - Vec2<float>(25.0f, 0.0f), Vec2<float>(size, size), 0.0f, TexHandleMgr::GetTexBuffer(number[maxNumber[i]]));
 	}
 }
 
 std::vector<int> BlockCount::CountNumber(int TIME)
 {
 	float score = TIME;
-	std::vector<int> Number(KuroFunc::GetDigit(TIME));
+	std::vector<int> Number(KuroFunc::GetDigit(countAllBlockNum));
 
 	int tmp = score;
 	//�X�R�A�v�Z
@@ -71,7 +74,7 @@ std::vector<int> BlockCount::CountNumber(int TIME)
 	//0����
 	for (int i = 0; i < Number.size(); i++)
 	{
-		if (Number[i] == -1)
+		if (Number[i] == 0)
 		{
 			Number[i] = 0;
 		}
