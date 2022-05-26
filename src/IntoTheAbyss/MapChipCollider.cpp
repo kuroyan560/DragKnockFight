e22d4f5b779ce1b65498563b806476d6a75596b4
hassy560 <k020g1167@g.neec.ac.jp>
2022-05-26 12:49:14 +0900

    プレイヤー、壊せない壁とのみ当たり判定とれるように

--- src/IntoTheAbyss/MapChipCollider.cpp
+++ src/IntoTheAbyss/MapChipCollider.cpp
@@ -58,7 +58,7 @@ Vec2<float> MapChipCollider::CalIntersectPoint(Vec2<float> posA1, Vec2<float> po
 	return Vec2<float>(posA1.x + (posA2.x - posA1.x) * t, posA1.y + (posA2.y - posA1.y) * t);
 }
 
-INTERSECTED_LINE MapChipCollider::CheckHitMapChipBasedOnTheVel(Vec2<float>& pos, const Vec2<float>& prevFramePos, const Vec2<float>& vel, const Vec2<float>& size, const vector<vector<int>>& mapChipData, Vec2<int>& hitChipIndex, bool OnlyMapFrame)
+INTERSECTED_LINE MapChipCollider::CheckHitMapChipBasedOnTheVel(Vec2<float>& pos, const Vec2<float>& prevFramePos, const Vec2<float>& vel, const Vec2<float>& size, const vector<vector<int>>& mapChipData, Vec2<int>& hitChipIndex, bool OnlyUnBrokenBlock)
 {
 	/*===== マップチップとプレイヤーの当たり判定 =====*/
 
@@ -74,10 +74,10 @@ INTERSECTED_LINE MapChipCollider::CheckHitMapChipBasedOnTheVel(Vec2<float>& pos,
 		const int WIDTH = mapChipData[height].size();
 		for (int width = 0; width < WIDTH; ++width) {
 
-			//マップ外側とのみ当たり判定を取る
-			if (OnlyMapFrame)
+			//壊れないブロックとのみ当たり判定を取る
+			if (OnlyUnBrokenBlock)
 			{
-				if (0 < width && width < WIDTH - 1 && 0 < height && height < HEIGHT - 1)continue;
+				if (mapChipData[height][width] != MAPCHIP_TYPE_STATIC_UNBROKEN_BLOCK)continue;
 			}
 
 			// マップIDが0だったら処理を飛ばす。
@@ -327,7 +327,7 @@ INTERSECTED_LINE MapChipCollider::CheckHitMapChipBasedOnTheVel(Vec2<float>& pos,
 	return INTERSECTED_NONE;
 }
 
-INTERSECTED_LINE MapChipCollider::CheckHitMapChipBasedOnTheScale(Vec2<float>& pos, const Vec2<float>& prevFramePos, const Vec2<float>& size, const vector<vector<int>>& mapChipData, const INTERSECTED_LINE& direction, Vec2<int>& hitChipIndex, bool OnlyMapFrame)
+INTERSECTED_LINE MapChipCollider::CheckHitMapChipBasedOnTheScale(Vec2<float>& pos, const Vec2<float>& prevFramePos, const Vec2<float>& size, const vector<vector<int>>& mapChipData, const INTERSECTED_LINE& direction, Vec2<int>& hitChipIndex, bool OnlyUnBrokenBlock)
 {
 	/*===== マップチップとプレイヤーの当たり判定 =====*/
 
@@ -384,10 +384,10 @@ INTERSECTED_LINE MapChipCollider::CheckHitMapChipBasedOnTheScale(Vec2<float>& po
 		const int WIDTH = mapChipData[height].size();
 		for (int width = 0; width < WIDTH; ++width) {
 
-			//マップ外側とのみ当たり判定を取る
-			if (OnlyMapFrame)
+			//壊れないブロックとのみ当たり判定を取る
+			if (OnlyUnBrokenBlock)
 			{
-				if (0 < width && width < WIDTH - 1 && 0 < height && height < HEIGHT - 1)continue;
+				if (mapChipData[height][width] != MAPCHIP_TYPE_STATIC_UNBROKEN_BLOCK)continue;
 			}
 
 			// マップIDが0だったら処理を飛ばす。
