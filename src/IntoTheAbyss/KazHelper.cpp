86a921fdceaef2cdb88d5978d3619dff26fa4306
LC1A_07_オオイシカズノリ <k020g1151@g.neec.ac.jp>
2022-05-30 12:23:55 +0900

    KazHelperのコミットし忘れ

---
+++ src/IntoTheAbyss/KazHelper.cpp
@@ -0,0 +1,37 @@
+#include "KazHelper.h"
+
+std::vector<int> KazHelper::CountNumber(int TIME, int ZERO)
+{
+	float score = TIME;
+
+	int haveZero = 0;
+
+	//0–„‚ß‚·‚é
+	if (ZERO != -1)
+	{
+		haveZero = ZERO;
+	}
+	else
+	{
+		haveZero = TIME;
+	}
+
+	std::vector<int> Number(haveZero);
+
+	int tmp = score;
+	for (int i = 0; tmp > 0; i++)
+	{
+		float result = tmp % 10;
+		Number[i] = result;
+		tmp /= 10.0f;
+	}
+	for (int i = 0; i < Number.size(); i++)
+	{
+		if (Number[i] == 0)
+		{
+			Number[i] = 0;
+		}
+	}
+	std::reverse(Number.begin(), Number.end());
+	return Number;
+}
---
+++ src/IntoTheAbyss/KazHelper.h
@@ -0,0 +1,11 @@
+#pragma once
+#include<vector>
+
+namespace KazHelper
+{
+
+	std::vector<int> CountNumber(int TIME, int ZERO = -1);
+
+
+}
+
