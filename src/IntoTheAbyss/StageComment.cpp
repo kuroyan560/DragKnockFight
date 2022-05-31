407a100ba4a1cf1c6362fbab2efe3900401e53eb
LC1A_07_オオイシカズノリ <k020g1151@g.neec.ac.jp>
2022-05-31 15:19:45 +0900

    画面端の半分だけスクロールを描画する処理を追加

--- src/IntoTheAbyss/StageComment.cpp
+++ src/IntoTheAbyss/StageComment.cpp
@@ -34,7 +34,9 @@ void StageComment::Update()
 		}
 		commentSprite->SetTexture(TexHandleMgr::GetTexBuffer(commentGraph));
 		commentSprite->mesh.SetUv(0.0f, 1.0f, 0.0f, 1.0f);
-		commentSprite->mesh.SetSize(TexHandleMgr::GetTexBuffer(commentGraph)->GetGraphSize().Float());
+
+		Vec2<float>data(TexHandleMgr::GetTexBuffer(commentGraph)->GetGraphSize().Float().x / 2.0f, TexHandleMgr::GetTexBuffer(commentGraph)->GetGraphSize().Float().y);
+		commentSprite->mesh.SetSize(data);
 		commentSprite->mesh.SetAnchorPoint({ 1.0f,0.0f });
 		COMMENT_MOVE_WAIT_TIMER = 0;
 		OLD_STAGE_NUM = stageNum;
@@ -45,7 +47,7 @@ void StageComment::Update()
 		const float SCROLL = 0.001f;
 		commentSprite->mesh.AddUv({ SCROLL,0.0f }, { SCROLL,0.0f }, { SCROLL,0.0f }, { SCROLL,0.0f });
 	}
-	commentSprite->transform.SetPos({ WinApp::Instance()->GetExpandWinSize().x,90.0f });
+	commentSprite->transform.SetPos({ WinApp::Instance()->GetExpandWinSize().x,680.0f });
 
 	//KuroMath::Lerp(0.0f, -128.0f, changeRate);
 }
@@ -53,9 +55,11 @@ void StageComment::Update()
 void StageComment::Draw()
 {
 	//DrawFunc::DrawBox2D({ 0,0 }, commentSprite->mesh.GetSize() * Vec2<float>(1.0f, 1.3f), Color(0.0f, 0.0f, 0.0f, 0.7f * KuroMath::Lerp(1.0f, 0.0f, changeRate)), true, AlphaBlendMode_Trans);
+	Vec2<float>size(commentSprite->mesh.GetSize() * Vec2<float>(1.0f, 1.3f));
+	Vec2<float>leftUpPos(commentSprite->transform.GetPos().x - size.x, commentSprite->transform.GetPos().y);
+	Vec2<float>rightDownPos(leftUpPos + size);
 
-	Vec2<float>leftUpPos(0.0f, 0.0f);
-	Vec2<float>rightDownPos(commentSprite->mesh.GetSize() * Vec2<float>(1.0f, 1.3f));
+
 	DrawFunc::DrawBox2D(leftUpPos, rightDownPos, Color(0.0f, 0.0f, 0.0f, 0.7f), true, AlphaBlendMode_Trans);
 	commentSprite->Draw();
 }
